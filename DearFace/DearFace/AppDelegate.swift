147cd86d75d42f5685a44c82a37337efa3714359
Joe <jihd@dream1024.com>
2016-08-31 11:39:44 +0800

    project

---
+++ DearFace/DearFace/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  DearFace
+//
+//  Created by Lucky on 16/2/22.
+//  Copyright © 2016年 Joker. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        window!.rootViewController = MakeFaceViewController()
+        window!.makeKeyAndVisible()
+        
+        return true
+    }
+
+
+}
+
