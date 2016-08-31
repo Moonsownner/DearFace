e325ccdd92e2aa94fe4f99efdac9ed814e45fc6a
Joe <jihd@dream1024.com>
2016-08-31 11:55:40 +0800

    目录结构调整

---
+++ DearFace/DearFace/Const.swift
@@ -0,0 +1,19 @@
+//
+//  Const.swift
+//  DearFace
+//
+//  Created by J HD on 8/31/16.
+//  Copyright © 2016 Joker. All rights reserved.
+//
+
+import UIKit
+
+///屏幕宽度
+var screenWidth: CGFloat{
+    return UIScreen.mainScreen().bounds.width
+}
+
+///屏幕高度
+var screenHeight: CGFloat{
+    return UIScreen.mainScreen().bounds.height
+}
---
+++ DearFace/DearFace/MakeFaceController.swift
@@ -0,0 +1,25 @@
+//
+//  MakeFaceController.swift
+//  DearFace
+//
+//  Created by Lucky on 16/2/22.
+//  Copyright © 2016年 Joker. All rights reserved.
+//
+
+import UIKit
+
+class MakeFaceController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+
+    }
+    
+    func makeUI(){
+        view.backgroundColor = UIColor.whiteColor()
+        
+    }
+    
+}
